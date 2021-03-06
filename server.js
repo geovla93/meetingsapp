6d5e4bba7ab897d47ae226f21bbfa119ff41b55b
geovla93 <geovla@icloud.com>
2021-03-06 16:20:08 +0200

    servers

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ router.route("/delete/:id").delete((req, res, next) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
+	app.use(express.static(path.join(__dirname, "client/build")));
 }
 
 app.listen(port, () => {
