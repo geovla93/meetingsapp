1bb2c3e3efd9d6b8efaa6cce93c50fc508e2a3ec
geovla93 <geovla@icloud.com>
2021-03-06 17:37:53 +0200

    server changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use("/", router);
